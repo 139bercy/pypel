75228d76bcd7f977342702ab5b7349d40a73ad20
loldebyte <qdimarellis@laposte.net>
2021-01-25 14:20:47 +0100

    refacto import_kibana_data

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -2,5 +2,6 @@ from pypel.processes import *
 from pypel.main import *
 from pypel.init_indice import *
 from pypel.clean_indice import *
+from pypel.import_kibana_data import *
 __all__ = ["BaseProcess", "ExcelProcess", "ProcessFactory", "process_into_elastic", "init_indice", "clean_indice",
-           "import_kibana_data"]
+           "import_into_kibana"]
--- pypel/import_kibana_data.py
+++ pypel/import_kibana_data.py
@@ -10,43 +10,39 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def import_kibana_data(path_to_exports, kibana_host_and_port):
-    for file in os.listdir(path_to_exports):
-        if "export" in file and file.endswith(".ndjson"):
-            import_file(file, kibana_host_and_port, path_to_exports)
+class _KibanaExporter:
+    def __init__(self, path_to_exports, kibana_host_and_port):
+        self.path_to_exports = path_to_exports
+        self.kibana_host_and_port = kibana_host_and_port
+
+    def import_file(self, file):
+        curlcommand = self.create_curl_command(file)
+        args = shlex.split(curlcommand)
+        proc = subprocess.Popen(args=args, stderr=subprocess.PIPE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        if out:
+            clean_out = str(out)[2:-1]
+            data = json.loads(clean_out)
+            if data.get("error", False):
+                logger.warning(f"Failed to import file \'{file}\'!")
+                logger.debug(f"Response message : {clean_out}")
+            else:
+                logger.info(f"Successfully created space {data.get('name')}")
+                logger.debug(f"Import data details : {clean_out}")
+
+    def create_curl_command(self, file):
+        path = os.path.join(file)
+        return ('curl -X POST '
+                + self.kibana_host_and_port
+                + '/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@'
+                + path)
+
+    def import_kibana_data(self):
+        for file in os.listdir(self.path_to_exports):
+            if "export" in file and file.endswith(".ndjson"):
+                self.import_file(file)
 
 
-def import_file(file, kibana_host_and_port, path_to_exports):
-    curlcommand = create_curl_command(path_to_exports, file, kibana_host_and_port)
-    args = shlex.split(curlcommand)
-    proc = subprocess.Popen(args=args, stderr=subprocess.PIPE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    if out:
-        clean_out = str(out)[2:-1]
-        data = json.loads(clean_out)
-        if data.get("error", False):
-            logger.warning(f"Failed to import file \'{file}\'!")
-            logger.debug(f"Response message : {clean_out}")
-        else:
-            logger.info(f"Successfully created space {data.get('name')}")
-            logger.debug(f"Import data details : {clean_out}")
-
-
-def create_curl_command(path_to_exports, file, kibana_host_and_port):
-    path = os.path.join(path_to_exports, file)
-    return ('curl -X POST '
-            + kibana_host_and_port
-            + '/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@'
-            + path)
-
-
-if __name__ == "__main__":
-    with open("./conf/config.json") as f:
-        conf = json.load(f)
-    with open("./conf/parameters.json") as f:
-        params = json.load(f)
-
-    path_to_kibana_exports = conf["path_to_kibana_exports"]
-    kibana_info = conf["kibana_info"]
-
-    import_kibana_data(path_to_kibana_exports, kibana_info)
+def import_into_kibana(path_to_exports, kibana_host_and_port):
+    exporter = _KibanaExporter(path_to_exports, kibana_host_and_port)
+    exporter.import_kibana_data()
