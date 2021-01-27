1c228aca16b662b9ecb5194dbcee9867bfc6a509
loldebyte <qdimarellis@laposte.net>
2021-01-27 10:11:05 +0100

    refacto factory

--- pypel/processes/ProcessFactory.py
+++ pypel/processes/ProcessFactory.py
@@ -1,17 +1,6 @@
 from pypel.processes.Process import Process as BaseProcess
-from pypel.processes.ExcelProcess import ExcelProcess
 
 
 class ProcessFactory:
-
-    def __init__(self):
-        self._processes = {
-            "DummyProcess": BaseProcess,
-            "DummyExcelProcess": ExcelProcess
-        }
-
-    def create_process(self, process_name, elastic_indice):
-        process = self._processes.get(process_name)
-        if not process:
-            raise ValueError(f"{process_name} not found in ProcessFactory")
-        return process(elastic_indice)
+    def create_process(self, *args, **kwargs):
+        return BaseProcess(*args, **kwargs)
