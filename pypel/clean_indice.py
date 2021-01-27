367029e4d55c2fd5cfb5cc64d1712385d30ee755
loldebyte <qdimarellis@laposte.net>
2021-01-27 12:28:26 +0100

    delete obsolete clean_aggregated_indices

--- pypel/clean_indice.py
+++ pypel/clean_indice.py
@@ -30,42 +30,8 @@ def clean_indice(mappings, es_indice_client, indices: str = "all"):
             logger.warning(f"Indice {indices} not found in index_mappings.json")
 
 
-def clean_aggregated_indice(agg_mappings, mappings, es_indice_client, indices: str = "all"):
-    if indices == "all":
-        for base_agg_indice_name in agg_mappings.keys():
-            agg_indices = [base_agg_indice_name + "_" + str(indice)
-                           for indice in mappings.keys()]
-
-            for agg_indice in agg_indices:
-                if (es_indice_client.exists(agg_indice)):
-                    es_indice_client.delete(agg_indice)
-                    logger.info(f"Indices {agg_indice} successfully deleted")
-                else:
-                    logger.warning(f"Indice {base_agg_indice_name} does not exist")
-    elif indices == "none":
-        return
-    else:
-        if indices in mappings.keys():
-            if es_indice_client.exists([indices]):
-                es_indice_client.delete(indices)
-                logger.info(f"Indices {indices} successfully deleted")
-            else:
-                logger.warning(f"Indice {indices} does not exist")
-
 def get_indice_range():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--indice-name", default="all", type=str, help="get the name of the Indice")
     args = parser.parse_args()
     return args.index_name
-
-
-
-if __name__ == "__main__":
-    with open("./exports_kibana/index_mappings.json") as f:
-        MAPPINGS = json.load(f)
-    with open("./conf/config.json") as f:
-        ip_conf = json.load(f)
-    ES = elasticsearch.Elasticsearch(hosts=ip_conf["elastic_ip"])
-    ES_INDICES_CLIENT = elasticsearch.client.IndicesClient(ES)
-
-    clean_indice(MAPPINGS, ES_INDICES_CLIENT, get_indice_range())
