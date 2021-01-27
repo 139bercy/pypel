803af9aee435b03a57048fc934e2cae2282e7d94
loldebyte <qdimarellis@laposte.net>
2021-01-27 12:26:39 +0100

    fix __init__.py s I am most unsatisfied by these changes

--- pypel/processes/__init__.py
+++ pypel/processes/__init__.py
@@ -1,4 +1,3 @@
 from pypel.processes.Process import Process as BaseProcess
-from pypel.processes.ExcelProcess import ExcelProcess
 from pypel.processes.ProcessFactory import ProcessFactory
-__all__ = ["BaseProcess", "ExcelProcess", "ProcessFactory"]
+__all__ = ["BaseProcess", "ProcessFactory"]
