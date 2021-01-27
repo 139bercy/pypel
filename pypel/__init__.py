58363ac4348fefbda0e0877d666d3a8af1d14e92
loldebyte <qdimarellis@laposte.net>
2021-01-27 10:11:46 +0100

    change __init__.py I am very unpleased with the 3 different from _ import class.

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -5,3 +5,8 @@ from pypel.clean_indice import *
 from pypel.import_kibana_data import *
 __all__ = ["BaseProcess", "ExcelProcess", "ProcessFactory", "process_into_elastic", "init_indice", "clean_indice",
            "import_into_kibana"]
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
