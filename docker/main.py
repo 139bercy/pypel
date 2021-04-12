e52b8900f87ab27ffe7de728664c83a860396b59
Alexis Charveriat <alexis.charveriat@elastic.co>
2021-04-12 12:19:48 +0200

    Updated dockerized main.py
    This will process all the given paths:
     - perform sanity checks
     - run the JSON content against the process_into_elastic function

--- docker/main.py
+++ docker/main.py
@@ -1,4 +1,8 @@
 import argparse
+import json
+
+from glob import glob
+from pypel import process_into_elastic
 
 
 def main():
@@ -14,8 +18,28 @@ def main():
 
     args = parser.parse_args()
 
-    # Just print what we should process for now
-    print(args.paths)
+    # Process all the given paths, we support wildcard expansion through glob
+    # https://docs.python.org/3/library/glob.html
+    for path in [glob(p) for p in args.paths]:
+
+        print(f"Processing {path}")
+
+        with open(path, "r") as f:
+            config = json.load(f)
+
+        expected_keys = ["conf", "params", "mappings"]
+
+        # Check if we're missing some expected keys
+        if set(expected_keys) - set(config.keys()):
+            print(f"Expected {expected_keys} in {path} - skipping")
+            continue
+
+        process_into_elastic(
+            conf=config["conf"],
+            params=config["params"],
+            mappings=config["mappings"],
+            process=config.get("process"),
+        )
 
 
 if __name__ == "__main__":
