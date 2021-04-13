996d14de6a2655660cb7d6a06ca56f543f49e73d
loldebyte <qdimarellis@laposte.net>
2021-04-13 16:44:52 +0200

    remove import made redundant by fetching the updates from master

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -3,7 +3,6 @@ from pypel.main import *
 from pypel.init_indice import *
 from pypel.clean_indice import *
 from pypel.import_kibana_data import *
-from pypel.transformers.Transformer import Transformer
 __all__ = ["Process", "ProcessFactory", "process_into_elastic", "init_indice", "clean_indice",
            "import_into_kibana", "Loader", "Transformer", "Extractor"]
 
