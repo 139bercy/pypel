6f950483fcecbd7d8e15663a0e50b9d7b2d53194
loldebyte <qdimarellis@laposte.net>
2021-05-17 08:00:07 +0200

    refactored Process.py to support both instanced and passed by class Extractors & Transformers

--- pypel/processes/Process.py
+++ pypel/processes/Process.py
@@ -1,6 +1,7 @@
 from pypel.extractors.Extractor import Extractor
 from pypel.transformers.Transformer import Transformer
 from pypel.loaders.Loader import Loader
+import warnings
 
 
 class Process:
@@ -16,20 +17,49 @@ class Process:
                  transformer=None,
                  loader=None):
         self.extractor = extractor if extractor else Extractor
-        assert isinstance(self.extractor, Extractor)
         self.transformer = transformer if transformer else Transformer
-        assert isinstance(self.transformer, Transformer)
         self.loader = loader if loader else Loader
-        assert isinstance(self.loader, Loader)
+        try:
+            assert isinstance(self.extractor, Extractor)
+            self.__extractor_is_instancied = True
+        except TypeError:
+            assert isinstance(self.extractor(), Extractor)
+            self.__extractor_is_instancied = False
+        except AssertionError as e:
+            warnings.warn("Invalid extractor !")
+            raise ValueError(e)
+        try:
+            assert isinstance(self.transformer, Transformer)
+            self.__transformer_is_instancied = True
+        except TypeError:
+            assert isinstance(self.transformer(), Transformer)
+            self.__transformer_is_instancied = False
+        except AssertionError as e:
+            warnings.warn("Invalid transformer !")
+            raise ValueError(e)
+        try:
+            # TODO: add support for instanced loaders
+            assert isinstance(self.loader("", ""), Loader)
+            self.__loader_is_instancied = False
+        except AssertionError as e:
+            warnings.warn("Invalid loader !")
+            raise ValueError(e)
 
     def process(self, file_path):
         self.load(self.transform(self.extract(file_path)))
 
     def extract(self, file_path, *args, **kwargs):
-        return self.extractor(*args, **kwargs).init_dataframe(file_path)
+        if self.__extractor_is_instancied:
+            return self.extractor.init_dataframe(file_path=file_path)
+        else:
+            return self.extractor(*args, **kwargs).init_dataframe(file_path)
 
     def transform(self, dataframe, *args, **kwargs):
-        return self.transformer(*args, **kwargs).transform(dataframe)
+        if self.__transformer_is_instancied:
+            return self.transformer.transform(dataframe=dataframe)
+        else:
+            return self.transformer(*args, **kwargs).transform(dataframe)
 
     def load(self, df, *args, **kwargs):
+        # instanced loaders not currently supported
         self.loader(*args, **kwargs).load(df)
