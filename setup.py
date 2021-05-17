c25b320a0666c938df806db291593b7d31cb5d83
loldebyte <qdimarellis@laposte.net>
2021-05-17 07:14:52 +0200

    add install_requires to install deps

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
-
 setup(name='pypel',
       version='0.1a',
       packages=["pypel", "pypel.processes"],
       author="Quentin Dimarellis",
-      author_email="quentin.dimarellis@finances.gouv.fr")
+      author_email="quentin.dimarellis@finances.gouv.fr",
+      install_requires=["pandas ~= 1.2.0",
+                        "elasticsearch",
+                        "openpyxl >= 3.0.0",
+                        "numpy ~= 1.19.1",
+                        "unidecode"])
