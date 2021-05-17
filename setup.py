33c1d4deaa93751d49178b3275087bc628d8321f
loldebyte <qdimarellis@laposte.net>
2021-05-17 07:17:39 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 setup(name='pypel',
-      version='0.1a',
-      packages=["pypel", "pypel.processes"],
+      version='0.1.2',
+      packages=["pypel"],
       author="Quentin Dimarellis",
       author_email="quentin.dimarellis@finances.gouv.fr",
-      install_requires=["pandas ~= 1.2.0",
+      install_requires=["pandas >= 1.2.0",
                         "elasticsearch",
                         "openpyxl >= 3.0.0",
-                        "numpy ~= 1.19.1",
+                        "numpy >= 1.19.1",
                         "unidecode"])
