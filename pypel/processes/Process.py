de279ec63339c2d5d56332fbf25148721c10723f
loldebyte <qdimarellis@laposte.net>
2021-05-25 16:02:47 +0200

    Document Process methods

--- pypel/processes/Process.py
+++ pypel/processes/Process.py
@@ -5,16 +5,38 @@ from pypel.loaders.Loader import Loader
 
 class Process:
     """
-    Default process that only manages .csv and performs minimal modifications :
-    - parse dates in columns `dates` according to format `dates_format`
-    - strip columns in `strip`
-    - uses pandas-type `converters` to read data
-    - exports data just before sending it to Elasticsearch's bulk API if `backup` is True.
+    Wrapper around dedicated E(xtract)/T(ransform)/L(oad) classes.
+
+    Parameters
+    ----------
+    :param extractor: pypel.Extractor
+        instance to use for extracting data. MUST be derived from pypel.Extractor
+    :param transformer: pypel.Transformer
+        instance to use for transforming data. MUST be derived from pypel.Transformer
+    :param loader: pypel.Loader
+        instance to use for loading data. MUST be derived from pypel.Loader
+
+    Examples
+    --------
+    Instanciate the default Process
+
+    >>> import pypel
+    >>> my_process = pypel.Process()
+
+    Instanciate a Process with a custom Extractor (same logic applies for custom Transformers/Loaders)
+
+    >>> class MyExtractor(pypel.Extractor):
+    ...       def __init__(self):
+    ...          super().__init__()
+    >>> my_process = pypel.Process(extractor=MyExtractor)
+    or
+    >>> my_extractor_instance = MyExtractor(converter={"columns1": float})
+    >>> my_process2 = pypel.Process(extractor=my_extractor_instance)
     """
     def __init__(self,
-                 extractor=None,
-                 transformer=None,
-                 loader=None):
+                 extractor: Extractor or type = None,
+                 transformer: Transformer or type = None,
+                 loader: type = None):
         self.extractor = extractor if extractor else Extractor
         self.transformer = transformer if transformer else Transformer
         self.loader = loader if loader else Loader
@@ -42,20 +64,60 @@ class Process:
             raise ValueError("Bad loader") from e
 
     def process(self, file_path, es_instance, es_indice):
+        """
+        Conveniance wrapper around Process.extract, Process.transform & Process.load
+
+        :param file_path:
+            path to the file to be extracted
+        :param es_instance:
+            elasticsearch connection to use for loading
+        :param es_indice:
+            the name of the elasticsearch indice in which data is to be loaded
+        :return: None
+        """
         self.load(self.transform(self.extract(file_path)), es_indice, es_instance)
 
     def extract(self, file_path, *args, **kwargs):
+        """
+
+        :param file_path:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         if self.__extractor_is_instancied:
             return self.extractor.init_dataframe(file_path=file_path)
         else:
             return self.extractor(*args, **kwargs).init_dataframe(file_path)
 
     def transform(self, dataframe, *args, **kwargs):
+        """
+
+        :param dataframe:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         if self.__transformer_is_instancied:
             return self.transformer.transform(dataframe=dataframe)
         else:
             return self.transformer(*args, **kwargs).transform(dataframe)
 
-    def load(self, df, *args, **kwargs):
+    def load(self, df, es_indice, es_instance, *args, **kwargs):
+        """
+        Loads the passed dataframe into the passed elasticsearch instance's indice es_indice.
+
+        :param df: pd.Dataframe
+            the dataframe to load
+        :param es_indice:
+            the elasticsearch indice in which to load
+        :param es_instance:
+            the elasticsearch instance in which to load
+        :param args:
+            optional parameters to pass to Loader
+        :param kwargs:
+            optional parameters to pass to Loader
+        :return:
+        """
         # instanced loaders not currently supported
-        self.loader(*args, **kwargs).load(df)
+        self.loader(es_indice, es_instance, *args, **kwargs).load(df)
