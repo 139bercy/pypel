821b2a1faa8348dcd85f665312386402ba2b24d6
loldebyte <qdimarellis@laposte.net>
2021-05-25 16:02:41 +0200

    Document Loader methods

--- pypel/loaders/Loader.py
+++ pypel/loaders/Loader.py
@@ -11,16 +11,27 @@ logger.setLevel(logging.DEBUG)
 
 
 class Loader:
+    """
+    Encapsulates all the loading logic.
+
+    :param indice: str
+        the elasticserach indice in which data is to be loaded
+    :param elasticsearch_instance: elasticsearch.Elasticsearch
+        the elasticsearch connection to load into. MUST be an instance of elasticsearch.Elasticsearch
+    :param path_to_export_folder: str
+    :param backup:
+    :param name_export:
+    :param dont_append_date:
+    """
     def __init__(self,
-                 indice,
-                 elasticsearch_instance,
+                 indice: str,
+                 elasticsearch_instance: elasticsearch.Elasticsearch,
                  path_to_export_folder=None,
                  backup=False,
                  name_export=None,
                  dont_append_date=False):
         if not path_to_export_folder and backup:
-            warnings.warn("No path for export but backup set to true !")
-            return
+            raise ValueError("No path for export but backup set to true !")
         self.indice = indice
         self.backup_uploaded_data = backup
         self.path_to_folder = path_to_export_folder
@@ -29,6 +40,13 @@ class Loader:
         self.append_date = not dont_append_date
 
     def load(self, dataframe: pd.DataFrame):
+        """
+        Load passed dataframe using current Loader's parameters
+
+        :param dataframe:
+            the dataframe to load
+        :return: None
+        """
         df = dataframe.copy()
         if self.backup_uploaded_data:
             self.export_csv(df)
@@ -36,6 +54,13 @@ class Loader:
         self.bulk_into_elastic(actions)
 
     def bulk_into_elastic(self, actions: list):
+        """
+        Attempts to load actions into elasticsearch using the bulk API.
+        Successful loads are logged, errors are sent as warnings
+
+        :param actions: a list of elasticsearch actions
+        :return: None
+        """
         success, failed, errors = 0, 0, []
         for ok, item in elasticsearch.helpers.streaming_bulk(self.es, actions, raise_on_error=False):
             if not ok:
@@ -52,6 +77,9 @@ class Loader:
         Reformats the dataframe object as a list of Elasticsearch actions, fit for elasticsearch's bulk API.
         If self.backup is True, save a copy of the dataframe as csv for debugging.
 
+        :param df: pd.DataFrame
+            the DataFrame to upload
+
         :return: returns a list of actions (cf Elasticsearch python API documentation)
         """
         logger.info(f"{len(df.index)} rows in the dataframe")
@@ -66,7 +94,12 @@ class Loader:
         return actions
 
     def export_csv(self, df: pd.DataFrame):
-        """Save a the dataframe as csv in a sub-directory of the process's data directory."""
+        """
+        Save a the dataframe as csv in TODO: fix and document
+
+        :param df: the dataframe to save
+        :return: None
+        """
         if not self.name_export_file:
             name_file = "exported_data_" + str(self.indice) + self.get_date() + ".csv"
         else:
