89f14ee080073c536e1869347392390a316f9826
loldebyte <qdimarellis@laposte.net>
2021-06-08 15:40:55 +0200

    feature - add a package-wide configuration & its API

---
+++ _config/config.py
@@ -0,0 +1,25 @@
+"""
+Package-wide configuration & its API
+
+Configuration is stored inside a dictionnary where the config's name is the key, and the parameter the value.
+Example :
+>>> {"LOGS": True}
+"""
+_conf = {"LOGS": True,
+         "LOGS_LEVEL": "DEBUG"}
+
+
+def set_config(**kwargs) -> None:
+    """
+    Updates _conf with the passed **keywords arguments**.
+
+    Example :
+    >>>set_config(LOGS=True, LOGS_LEVEL="CRITICAL")
+    Sets `pypel`'s logs level to `logging.CRITICAL`
+    """
+    _conf.update(kwargs)
+
+
+def get_config() -> dict:
+    """Returns a **soft** copy of the configuration. You should never modify `get_config`'s return directly."""
+    return _conf
