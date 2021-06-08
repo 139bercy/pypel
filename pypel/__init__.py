7384575c61206d7716f88124eb766716a34697bc
loldebyte <qdimarellis@laposte.net>
2021-06-08 15:41:11 +0200

    feature - make package-wide configuration API available

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -3,10 +3,12 @@ from pypel.main import *
 from pypel.init_indice import *
 from pypel.clean_indice import *
 from pypel.import_kibana_data import *
+from _config.config import set_config, get_config
 __all__ = ["Process", "ProcessFactory", "process_into_elastic", "init_indice", "clean_indice",
-           "import_into_kibana", "Loader", "Transformer", "Extractor", "logger"]
+           "import_into_kibana", "Loader", "Transformer", "Extractor", "logger", "set_config", "get_config"]
 
-import logging
+if get_config()["LOGS"]:
+    import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(getattr(logging, get_config()["LOGS_LEVEL"]))
