8f548728383e5259248f5e537034b38cb8f573e2
loldebyte <qdimarellis@laposte.net>
2021-06-08 17:25:37 +0200

    refacto - pass indice to methods instead of __init__

--- pypel/loaders/Loader.py
+++ pypel/loaders/Loader.py
@@ -21,8 +21,6 @@ class Loader(BaseLoader):
     """
     Encapsulates all the loading logic.
 
-    :param indice: str
-        the elasticserach indice in which data is to be loaded
     :param elasticsearch_instance: elasticsearch.Elasticsearch
         the elasticsearch connection to load into. MUST be an instance of elasticsearch.Elasticsearch
     :param path_to_export_folder: str
@@ -31,7 +29,6 @@ class Loader(BaseLoader):
     :param dont_append_date:
     """
     def __init__(self,
-                 indice: str,
                  elasticsearch_instance: elasticsearch.Elasticsearch,
                  path_to_export_folder=None,
                  backup=False,
@@ -42,33 +39,36 @@ class Loader(BaseLoader):
                 raise ValueError("No export folder passed but backup set to true !")
             if not os.path.isdir(path_to_export_folder):
                 raise ValueError("Export folder does not exist but backup set to true !")
-        self.indice = indice
         self.backup_uploaded_data = backup
         self.path_to_folder = path_to_export_folder
         self.name_export_file = name_export
         self.es = elasticsearch_instance
         self.append_date = not dont_append_date
 
-    def load(self, dataframe: pd.DataFrame):
+    def load(self, dataframe: pd.DataFrame, indice: str):
         """
         Load passed dataframe using current Loader's parameters
 
         :param dataframe:
             the dataframe to load
+        :param indice:
+            the elasticsearch index in which to upload
         :return: None
         """
         df = dataframe.copy()
         if self.backup_uploaded_data:
-            self._export_csv(df)
-        actions = self._wrap_df_in_actions(df)
-        self._bulk_into_elastic(actions)
+            self._export_csv(df, indice)
+        actions = self._wrap_df_in_actions(df, indice)
+        self._bulk_into_elastic(actions, indice)
 
-    def _bulk_into_elastic(self, actions: list):
+    def _bulk_into_elastic(self, actions: list, indice: str):
         """
         Attempts to load actions into elasticsearch using the bulk API.
         Successful loads are logged, errors are sent as warnings
 
         :param actions: a list of elasticsearch actions
+        :param indice: str
+            the elasticserach indice in which data is to be loaded
         :return: None
         """
         success, failed, errors = 0, 0, []
@@ -78,42 +78,45 @@ class Loader(BaseLoader):
                 failed += 1
             else:
                 success += 1
-        logging.info(f"{success} successfully inserted into {self.indice}")
+        logging.info(f"{success} successfully inserted into {indice}")
         if errors:
             warnings.warn(f"{failed} errors detected\nError details : {errors}")
 
-    def _wrap_df_in_actions(self, df: pd.DataFrame):
+    def _wrap_df_in_actions(self, df: pd.DataFrame, indice: str):
         """
         Reformats the dataframe object as a list of Elasticsearch actions, fit for elasticsearch's bulk API.
         If self.backup is True, save a copy of the dataframe as csv for debugging.
 
         :param df: pd.DataFrame
             the DataFrame to upload
-
+        :param indice: str
+            the indice in which to upload
         :return: returns a list of actions (cf Elasticsearch python API documentation)
         """
         logger.info(f"{len(df.index)} rows in the dataframe")
         data_dict = df.to_dict(orient="index")
         actions = [
             {
-                "_index": self.indice + self._get_date() if self.append_date else self.indice,
+                "_index": indice + self._get_date() if self.append_date else indice,
                 "_source": value
             }
             for value in data_dict.values()
         ]
         return actions
 
-    def _export_csv(self, df: pd.DataFrame, sep: str = '|'):
+    def _export_csv(self, df: pd.DataFrame, indice: str, sep: str = '|'):
         """
         Appends the dataframe to the csv located in the loader's backup folder `self.path_to_folder`, creating said csv
             if missing. This means a single Loader has a single backup file, regardless of how many dataframes it loads.
 
         :param df: the dataframe to save
+        :param indice: str
+            the elasticserach indice in which data is to be loaded
         :param sep: a single character to use as separator in the resulting csv file
         :return: None
         """
         if not self.name_export_file:
-            name_file = "exported_data_" + str(self.indice) + self._get_date() + ".csv"
+            name_file = "exported_data_" + str(indice) + self._get_date() + ".csv"
         else:
             name_file = self.name_export_file
         path_to_csv = os.path.join(self.path_to_folder, name_file)
