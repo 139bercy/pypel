e4bc64f06776f906a93a4d5074fe31511cc953d1
loldebyte <qdimarellis@laposte.net>
2021-06-08 16:21:03 +0200

    refacto - increment patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ except ImportError:
 
 
 setup(name='pypel',
+<<<<<<< HEAD
       version='0.1.7',
+=======
+      version='0.1.6',
+>>>>>>> refacto - increment patch version
       description="PYthon Pipeline into ELasticsearch",
       packages=["pypel"],
       author="Quentin Dimarellis",
