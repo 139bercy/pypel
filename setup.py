641f1100cd3522093383d14a25e5647ee33e1030
loldebyte <qdimarellis@laposte.net>
2021-06-08 17:15:43 +0200

    refacto - cleanup

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ except ImportError:
 
 
 setup(name='pypel',
-<<<<<<< HEAD
       version='0.1.7',
-=======
-      version='0.1.6',
->>>>>>> refacto - increment patch version
       description="PYthon Pipeline into ELasticsearch",
       packages=["pypel"],
       author="Quentin Dimarellis",
