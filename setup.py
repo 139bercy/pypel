63fe3b412fdc6df6c6713daf99ef72116e38ec96
loldebyte <qdimarellis@laposte.net>
2021-06-15 10:26:40 +0200

    ci - sabotage setup.py to test the CI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 setup(name='pypel',
       version='0.2.1',
       description="PYthon Pipeline into ELasticsearch",
-      packages=["pypel", "_config"],
+      packages=["_config"],
       author="Quentin Dimarellis",
       author_email="quentin.dimarellis@finances.gouv.fr",
       install_requires=["pandas >= 1.2.0",
