caa2a18425b00ecf8046392286c1eb2885fcfd66
loldebyte <qdimarellis@laposte.net>
2021-06-16 11:21:03 +0200

    refacto - change instance params into method params

--- pypel/extractors/Extractor.py
+++ pypel/extractors/Extractor.py
@@ -14,34 +14,26 @@ if get_config().get("LOGS"):
 
 class Extractor:
     """
-    Encapsulates all the extracting, getting data logic. Most parameters are pandas.read_csv or .read_excel parameters
-
-    :param converters:
-        cf [pandas' doc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
-    :param dates: this is equivalent to pandas' `parse_dates` in
-         [read_excel](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
-    :param dates_format:
-        the date format that will be used when reading data
-    :param sheet_name:
-        cf [pandas' doc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
-    :param skiprows:
-        cf [pandas' doc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
-    :param kwargs:
+    Encapsulates all the extracting, getting data logic.
     """
-    def __init__(self, converters=None, dates=False, dates_format='%Y-%m-%d', sheet_name=0, skiprows=None, **kwargs):
-        self.converters = converters
-        self.dates = dates
-        self.dates_format = dates_format
-        self.sheet_name = sheet_name
-        self.skiprows = skiprows
-        self.additional_pandas_args = kwargs
-
-    def init_dataframe(self, file_path: str):
+    def init_dataframe(self, file_path: str, converters=None, dates=False, sheet_name=0,
+                       skiprows=None, **kwargs):
         """
         Read the passed file and return it as a dataframe.
         Uses many pandas parameters defined at Extractor instanciation.
 
         :param file_path: absolute path to the file
+        :param converters:
+            cf [pandas' doc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
+        :param dates: this is equivalent to pandas' `parse_dates` in
+             [read_excel](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
+        :param dates_format:
+            the date format that will be used when reading data
+        :param sheet_name:
+            cf [pandas' doc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
+        :param skiprows:
+            cf [pandas' doc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_excel.html)
+        :param kwargs: additional pandas args
         :return: pandas.Dataframe object
         """
         if file_path.endswith(".csv"):
@@ -52,18 +44,17 @@ class Extractor:
                 logger.debug(f"{row_count} rows (including header)"
                              f"detected in the csv {file_name}")
             return pd.read_csv(file_path,
-                               converters=self.converters,
-                               parse_dates=self.dates,
-                               **self.additional_pandas_args)
+                               converters=converters,
+                               parse_dates=dates,
+                               **kwargs)
         elif file_path.endswith(".xlsx"):
             if get_config()["LOGS"]:
                 wb = openpyxl.load_workbook(filename=file_path)
-                if self.sheet_name != 0:
-                    sheet = wb[self.sheet_name]
-                    self.sheet = self.sheet_name
+                if sheet_name != 0:
+                    sheet = wb[sheet_name]
                 else:
                     sheet = wb[wb.sheetnames[0]]
-                    self.sheet = sheet.title
+                    sheet_name = sheet.title
                 excel_rows = sheet.max_row
                 try:
                     file_name = re.findall(r"(?<=/)[.\s\w_-]+$", file_path)[0]
@@ -71,26 +62,22 @@ class Extractor:
                     warnings.warn(f"Could not get file name from file path :"
                                   f"{file_path}")
                     file_name = "ERROR"
-                logger.debug(f"{excel_rows} rows in the excel sheet \'{self.sheet}\'   from file \'{file_name}\'")
-            if self.skiprows is None:
-                skiprows = self.skiprows
-            else:
-                skiprows = arrayer(self.skiprows)
+                logger.debug(f"{excel_rows} rows in the excel sheet \'{sheet_name}\'   from file \'{file_name}\'")
+            if skiprows is not None:
+                skiprows = arrayer(skiprows)
             return pd.read_excel(io=file_path,
                                  skiprows=skiprows,
-                                 sheet_name=self.sheet_name,
-                                 converters=self.converters,
-                                 **self.additional_pandas_args)
+                                 sheet_name=sheet_name,
+                                 converters=converters,
+                                 **kwargs)
         elif file_path.endswith(".xls"):
-            if self.skiprows is None:
-                skiprows = self.skiprows
-            else:
-                skiprows = arrayer(self.skiprows)
+            if skiprows is not None:
+                skiprows = arrayer(skiprows)
             return pd.read_excel(io=file_path,
                                  skiprows=skiprows,
-                                 sheet_name=self.sheet_name,
-                                 converters=self.converters,
-                                 **self.additional_pandas_args,
-                                 engine="xlrd")
+                                 sheet_name=sheet_name,
+                                 converters=converters,
+                                 engine="xlrd",
+                                 **kwargs)
         else:
             raise ValueError("File has unsupported file extension")
