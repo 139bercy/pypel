a8a53be725eed53d180e7df24a13701012d88610
loldebyte <qdimarellis@laposte.net>
2021-06-16 10:57:46 +0200

    test - Extractor unit testing

---
+++ tests/test_extractor.py
@@ -0,0 +1,64 @@
+import pytest
+from pypel import Extractor
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import os
+
+
+@pytest.fixture
+def ex():
+    return Extractor()
+
+
+class TestExtractor:
+    def test_raises_if_unsupported_file_extension(self, ex):
+        with pytest.raises(ValueError):
+            ex.init_dataframe("./setup.py")
+
+    def test_init_dataframe_excel(self, ex):
+        path = os.path.join(os.getcwd(), "tests", "fake_data", "test_init_df.xlsx")
+        expected_default = pd.DataFrame(data=[[1, 1, 1, 1, 1],
+                                              [2, 2, 2, 2, 2],
+                                              [3, 3, 3, 3, 3],
+                                              [4, 4, 4, 4, 4],
+                                              [5, 5, 5, 5, 5],
+                                              [6, 6, 6, 6, 6],
+                                              [7, 7, 7, 7, 7],
+                                              [8, 8, 8, 8, 8],
+                                              [9, 9, 9, 9, 9]], columns=["a", "b", "c", "d", "e"])
+        df = ex.init_dataframe(path)
+        assert_frame_equal(expected_default, df)
+
+    def test_init_dataframe_excel_skiprows(self, ex):
+        path = os.path.join(os.getcwd(), "tests", "fake_data", "test_init_df.xlsx")
+        expected_skip_5 = pd.DataFrame(data=[[6, 6, 6, 6, 6],
+                                             [7, 7, 7, 7, 7],
+                                             [8, 8, 8, 8, 8],
+                                             [9, 9, 9, 9, 9]],
+                                       columns=["0", "1", "2", "3", "4"])
+
+        df = ex.init_dataframe(path, skiprows=5, header=None)
+        assert_frame_equal(expected_skip_5, df)
+
+    def test_init_dataframe_excel_sheetname(self):
+        path = os.path.join(os.getcwd(), "tests", "fake_data", "test_init_df.xlsx")
+        expected_sheetname = pd.DataFrame(data=[[9]], columns=["a"])
+        ex = Extractor(sheet_name="TEST")
+        df = ex.init_dataframe(path)
+        with pytest.warns(UserWarning):
+            obtained_sheetname = ex.transform(df)
+        assert_frame_equal(expected_sheetname, obtained_sheetname)
+
+    def test_init_dataframe_excel_csv(self, ex):
+        path_csv = os.path.join(os.getcwd(), "tests", "fake_data", "test_init_df.csv")
+        expected_csv = pd.DataFrame(data=[[1, 1, 1, 1, 1],
+                                          [2, 2, 2, 2, 2],
+                                          [3, 3, 3, 3, 3],
+                                          [4, 4, 4, 4, 4],
+                                          [5, 5, 5, 5, 5],
+                                          [6, 6, 6, 6, 6],
+                                          [7, 7, 7, 7, 7],
+                                          [8, 8, 8, 8, 8],
+                                          [9, 9, 9, 9, 9]], columns=["a", "b", "c", "d", "e"])
+        df = ex.init_dataframe(path_csv)
+        assert_frame_equal(expected_csv, df)
