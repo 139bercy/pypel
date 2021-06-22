7219ca9e9398e614976d15013203c24ba11874b0
loldebyte <qdimarellis@laposte.net>
2021-06-22 10:39:20 +0200

    refacto - remove condition that is never false

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -11,8 +11,7 @@ from pypel._config.config import set_config, get_config
 __all__ = ["Process", "ProcessFactory", "process_into_elastic", "init_index", "clean_index",
            "Loader", "Transformer", "Extractor", "logger", "set_config", "get_config"]
 
-if get_config()["LOGS"]:
-    import logging
+import logging
 
-    logger = logging.getLogger(__name__)
-    logger.setLevel(getattr(logging, get_config()["LOGS_LEVEL"]))
+logger = logging.getLogger(__name__)
+logger.setLevel(getattr(logging, get_config()["LOGS_LEVEL"]))
