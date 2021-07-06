251e01e058ff00981f32dbeb73f86f2fcbe8ac10
loldebyte <qdimarellis@laposte.net>
2021-07-06 11:08:10 +0200

    refacto - ProcessFactory.py now instanciates E/T/L classes first and uses instanciation parameters written in the configuration

--- pypel/processes/ProcessFactory.py
+++ pypel/processes/ProcessFactory.py
@@ -1,11 +1,5 @@
 import importlib
-from pypel.transformers.Transformer import Transformer
 from pypel.processes.Process import Process
-
-
-class MinimalTransformer(Transformer):
-    def format_str_columns(self, df):
-        pass
 
 
 class ProcessFactory:
@@ -13,17 +7,27 @@ class ProcessFactory:
     def __init__(self, path_to_refs=None):
         self.path_to_refs = path_to_refs
 
-    def create_process(self, config, index_pattern):
-        canonical_class_name = config["class"]
+    def create_process(self, config):
+        extractors = self.create_subclasses(config.get("Extractors"))
+        transformers = self.create_subclasses(config.get("Transformers"))
+        loaders = self.create_subclasses(config.get("Loaders"))
+        return Process(extractor=extractors,
+                       transformer=transformers,
+                       loader=loaders)
+
+    def create_subclasses(self, config):
+        if isinstance(config, list):
+            return [self.create_single_class(conf) for conf in config]
+        else:
+            return self.create_single_class(config)
+
+    def create_single_class(self, config):
+        canonical_class_name = config.pop("name")
         class_name_splitted = canonical_class_name.split('.')
         class_name = class_name_splitted[-1]
         module_name = ".".join(class_name_splitted[:-1])
-
         mod = importlib.import_module(module_name)
         klass = getattr(mod, class_name)
         if not klass:
             raise ValueError(f"{canonical_class_name} not found in ProcessFactory")
-        if Process == klass:
-            return Process(transformer=MinimalTransformer)
-        else:
-            return klass(index_pattern, path_to_refs=self.path_to_refs)
+        return klass(**config)
