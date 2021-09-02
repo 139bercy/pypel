710fe3723e8cecf68a848bea5a97f114e98dfa83
loldebyte <qdimarellis@laposte.net>
2021-09-02 11:35:39 +0200

    test: fix config tests that were expecting the old debug level

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -3,14 +3,14 @@ from pypel import get_config, set_config
 
 
 def test_default_config_getter():
-    assert get_config() == {"LOGS": True, "LOGS_LEVEL": "DEBUG"}
+    assert get_config() == {"LOGS": True, "LOGS_LEVEL": "INFO"}
 
 
 def test_config_setter():
     set_config(LOGS=False)
-    assert get_config() == {"LOGS": False, "LOGS_LEVEL": "DEBUG"}
+    assert get_config() == {"LOGS": False, "LOGS_LEVEL": "INFO"}
     set_config(LOGS=True)
 
 
 def test_disable_logs_fixture(disable_logs):
-    assert get_config() == {"LOGS": False, "LOGS_LEVEL": "DEBUG"}
+    assert get_config() == {"LOGS": False, "LOGS_LEVEL": "INFO"}
