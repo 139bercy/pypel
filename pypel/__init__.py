d19540db9a1c915bddd38d837f758be496e103a3
loldebyte <qdimarellis@laposte.net>
2021-09-03 16:02:36 +0200

    refacto!: remove obsolete imports, make BaseLoader & BaseTransformer available
    BREAKING CHANGE: MinimalTransformer no longer exists. Its functionality is now made available through BaseTransformer

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -1,15 +1,15 @@
 from pypel.processes.Process import Process
 from pypel.processes.ProcessFactory import ProcessFactory
-from pypel.transformers.Transformer import Transformer, MinimalTransformer
-from pypel.loaders.Loader import Loader
+from pypel.transformers.Transformer import Transformer, BaseTransformer
+from pypel.loaders.Loader import Loader, BaseLoader
 from pypel.extractors.Extractor import Extractor
 from pypel.main import process_from_config
 from pypel.utils.elk.init_index import *
 from pypel.utils.elk.clean_index import *
 from pypel._config.config import set_config, get_config
 
-__all__ = ["Process", "ProcessFactory", "process_from_config", "init_index", "clean_index",
-           "Loader", "Transformer", "Extractor", "logger", "set_config", "get_config", "MinimalTransformer"]
+__all__ = ["Process", "ProcessFactory", "process_from_config", "init_index", "clean_index", "BaseTransformer",
+           "Loader", "Transformer", "Extractor", "logger", "set_config", "get_config", "BaseTransformer"]
 
 import logging
 
