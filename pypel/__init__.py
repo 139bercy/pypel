8f3d159dd1bdb1edd2b842fd3bd5f2b337557827
loldebyte <qdimarellis@laposte.net>
2021-09-03 16:50:36 +0200

    test: fix tests broken by MinimalTransformer's deletion.

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -1,6 +1,6 @@
 from pypel.processes.Process import Process
 from pypel.processes.ProcessFactory import ProcessFactory
-from pypel.transformers.Transformer import Transformer, BaseTransformer
+from pypel.transformers.Transformer import Transformer, BaseTransformer, ColumnStripperTransformer
 from pypel.loaders.Loader import Loader, BaseLoader
 from pypel.extractors.Extractor import Extractor
 from pypel.main import process_from_config
@@ -8,8 +8,9 @@ from pypel.utils.elk.init_index import *
 from pypel.utils.elk.clean_index import *
 from pypel._config.config import set_config, get_config
 
-__all__ = ["Process", "ProcessFactory", "process_from_config", "init_index", "clean_index", "BaseTransformer",
-           "Loader", "Transformer", "Extractor", "logger", "set_config", "get_config", "BaseTransformer"]
+__all__ = ["Process", "ProcessFactory", "process_from_config", "init_index", "clean_index", "BaseTransformer", "Loader",
+           "Transformer", "Extractor", "logger", "set_config", "get_config", "BaseTransformer",
+           "ColumnStripperTransformer"]
 
 import logging
 
