28882ad2a125d4114d53fd091fe899ae30b43a8d
loldebyte <qdimarellis@laposte.net>
2021-09-06 14:38:16 +0200

    style: fix type-hinting of return types and various reformating

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,2 @@
-# obligé de la rajouter si on veut tester avec la commande "pytest" sinon il irra chercher les import dans le dossier d'instalation de pytest
+# obligé de la rajouter si on veut tester avec la commande "pytest"
+# sinon il irra chercher les import dans le dossier d'instalation de pytest
