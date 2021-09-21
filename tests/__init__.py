e355494ed84da33809eb2b1f695465757f2a481b
loldebyte <qdimarellis@laposte.net>
2021-09-21 18:08:10 +0200

    docs: fix typo

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
 # obligé de la rajouter si on veut tester avec la commande "pytest"
-# sinon il irra chercher les import dans le dossier d'instalation de pytest
+# sinon il ira chercher les import dans le dossier d'instalation de pytest
