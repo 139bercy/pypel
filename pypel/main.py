4dce3a0fb06488f5a4cce577848e99af7a4b5dc2
loldebyte <qdimarellis@laposte.net>
2021-09-29 15:40:33 +0200

    refacto!: change main according to 540b1f8d0691feee74177950fb1300bfdd2e9f7e
    main no longer relies on ES
    BREAKING CHANGE: indice can no longer be overloaded from the command line

--- pypel/main.py
+++ pypel/main.py
@@ -2,49 +2,32 @@ import json
 import os
 import pathlib
 import sys
-import elasticsearch
 import argparse
-import ssl
-from pypel.ProcessFactory import ProcessFactory
+from pypel.ProcessFactory import ProcessFactory, ProcessConfig
 # import pypel.utils.elk.clean_index as clean_index
 # import pypel.utils.elk.init_index as init_index
 import logging.handlers
-from typing import List, Dict, TypedDict, Union, Optional
+from typing import List, TypedDict, Union
 
 logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get("PYPEL_LOGS", "INFO"))
-
-
-class ProcessConfigMandatory(TypedDict):
-    Extractor: Dict[str, str]
-    Transformers: Union[Dict[str, str], List[Dict[str, str]]]
-    Loader: Dict[str, str]
-    indice: str
-
-
-class ProcessConfig(ProcessConfigMandatory):
-    name: str
 
 
 class Config(TypedDict):
     Processes: List[ProcessConfig]
 
 
-def select_process_from_config(es_: elasticsearch.Elasticsearch,
-                               processes: Config,
+def select_process_from_config(processes: Config,
                                process: str,
-                               files: Union[pathlib.Path, str],
-                               indice: Optional[str]):
+                               files: Union[pathlib.Path, str]):
     """
     Given a pair of configurations (global & process configuration `conf`and mapping configuration
     `mappings`), load all processes related to `process`, or all of them if `process` is omitted in to the Elasticsearch
     specified in `conf.elastic_ip`
 
-    :param es_: the elasticsearch instance
     :param processes: the configuration file
     :param process: the process to execute
     :param files: file or list of files to load
-    :param indice: the indice to load into
     :return: does not return
     """
     if processes is None:
@@ -57,36 +40,28 @@ def select_process_from_config(es_: elasticsearch.Elasticsearch,
                              "have a single Process") from e_
     if process == "all":
         for proc in processes:
-            process_from_config(es_, proc, files, indice)
+            process_from_config(proc, files)
     else:
         if process in [conf.get("name") for conf in processes]:
-            process_from_config(es_, [conf for conf in processes if conf.get("name") == process][0], files, indice)
+            process_from_config([conf for conf in processes if conf.get("name") == process][0], files)
         else:
             raise ValueError(f"process {process} not found in the configuration file !")
 
 
-def process_from_config(es_: elasticsearch.Elasticsearch, process: ProcessConfig, files: Union[pathlib.Path, str],
-                        indice: Optional[str]):
+def process_from_config(process: ProcessConfig, files: Union[pathlib.Path, str]):
     """
     Instantiate the process from its passed configuration, and execute it on passed file or directory
 
-    :param es_: the elasticsearch instance
     :param process: the configuration of the process to instantiate
     :param files: the file or directory to process (on)
-    :param indice: the elasticsearch index in which to load data
     :return: None
     """
-    if indice:
-        indice_ = indice
-    else:
-        indice_ = process["indice"]
-    processor = ProcessFactory().create_process(process, es_)
+    processor = ProcessFactory().create_process(process)
     if os.path.isdir(files):
         file_paths = [os.path.join(files, f_).__str__() for f_ in os.listdir(files)]
-        bulk_op = {indice_: file_paths}
-        processor.bulk(bulk_op)
+        processor.bulk(file_paths)
     elif os.path.isfile(files):
-        processor.process(files, indice_, es_)
+        processor.process(files)
     else:
         raise FileNotFoundError(f"Could not find file {files}")
 
@@ -103,31 +78,7 @@ def get_args(args_):
     #  parser.add_argument("-m", "--mapping", default="./conf/index_mappings.json", type=pathlib.Path,
     #                    help="path to the elasticsearch indices's mappings")
     parser.add_argument("-p", "--process", default="all", help="specify the process(es) to execute")
-    parser.add_argument("-i", "--indice", default=None, help="specify the indice to load into")
     return parser.parse_args(args_)
-
-
-def get_es_instance(conf):
-    """Instanciates an Elasticsearch connection instance based on connection parameters from the configuration"""
-    _host = (conf.get("user"), conf.get("pwd"))
-    if _host == (None, None):
-        _host = None
-    if "cafile" in conf:
-        _context = ssl.create_default_context(cafile=conf["cafile"])
-        es_instance = elasticsearch.Elasticsearch(
-            conf.get("host", "localhost"),
-            http_auth=_host,
-            use_ssl=True,
-            scheme=conf["scheme"],
-            port=conf["port"],
-            ssl_context=_context,
-        )
-    else:
-        es_instance = elasticsearch.Elasticsearch(
-            conf.get("host", "localhost"),
-            http_auth=_host,
-        )
-    return es_instance
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -140,7 +91,6 @@ if __name__ == "__main__":  # pragma: no cover
             config = json.load(f)
     except FileNotFoundError as e:
         raise ValueError("Cannot find file passed through the -c / --config-file argument") from e
-    es = get_es_instance(config)
     """
     if args.clean:
         path_to_mapping = os.path.join(os.getcwd(), args.mapping)
@@ -154,4 +104,4 @@ if __name__ == "__main__":  # pragma: no cover
         init_index.init_index(mappings, es_index_client)
     """
     logger.debug(config.get("Processes"))
-    select_process_from_config(es, config.get("Processes"), args.process, args.source_path, args.indice)
+    select_process_from_config(config.get("Processes"), args.process, args.source_path)
