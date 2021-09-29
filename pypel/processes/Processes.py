3706c5fe607f3c1ec64339b1555f1a48c286bac9
loldebyte <qdimarellis@laposte.net>
2021-09-29 15:45:58 +0200

    refacto: change process according to 540b1f8d0691feee74177950fb1300bfdd2e9f7e

--- pypel/processes/Processes.py
+++ pypel/processes/Processes.py
@@ -2,9 +2,8 @@ import os
 from pypel.extractors.Extractors import BaseExtractor, CSVExtractor
 from pypel.transformers.Transformers import Transformer, BaseTransformer
 from pypel.loaders.Loaders import Loader, BaseLoader
-from elasticsearch import Elasticsearch
 import warnings
-from typing import Dict, List, Union, Optional
+from typing import List, Union, Optional
 from pandas import DataFrame
 
 
@@ -15,14 +14,13 @@ class Process:
     Parameters
     ----------
     :param extractor: Optional[pypel.extractors.Extractor]
-        instance or class to use for extracting data. MUST be derived from pypel.Extractor
+        Extractor instance to use for extracting data. MUST be derived from pypel.extractors.BaseExtractor
     :param transformer: Union[pypel.BaseTransformer, type, List[pypel.transformers.BaseTransformers], None]
         instance, class or list of instances to use for transforming data.
         MUST be derived from pypel.Transformer, for lists, all elements of the list must inherit from pypel.Transformer.
         if list-like, will be for-in looped on, so mind the order.
-
     :param loader: Union[pypel.loaders.BaseLoader, type, None]
-        class to use for loading data. MUST be derived from pypel.Loader
+        Loader instance or class to use for loading data. MUST be derived from pypel.loaders.BaseLoader
 
     Examples
     --------
@@ -67,7 +65,7 @@ class Process:
             raise ValueError("Bad transformer") from e
         try:
             try:
-                assert isinstance(self.loader(None), BaseLoader) # noqa
+                assert issubclass(self.loader, BaseLoader)
                 self.__loader_is_instanced = False
             except TypeError:
                 assert isinstance(self.loader, BaseLoader)
@@ -75,20 +73,15 @@ class Process:
         except AssertionError as e:
             raise ValueError("Bad loader argument") from e
 
-    def process(self, file_path: Union[str, bytes, os.PathLike], es_indice: Optional[str],
-                es_instance: Optional[Elasticsearch] = None) -> None:
+    def process(self, file_path: Union[str, bytes, os.PathLike]) -> None:
         """
-        Conveniance wrapper around Process.extract, Process.transform & Process.load
+        Conveniance wrapper around Process.extract, Process.transform & Process.load. Relies on instanced E/T/L classes.
 
         :param file_path:
             path to the file to be extracted
-        :param es_instance:
-            elasticsearch connection to use for loading
-        :param es_indice:
-            the name of the elasticsearch indice in which data is to be loaded
         :return: None
         """
-        self.load(self.transform(self.extract(file_path)), es_indice, es_instance=es_instance)
+        self.load(self.transform(self.extract(file_path)))
 
     def extract(self, file_path: Union[str, bytes, os.PathLike], **kwargs) -> DataFrame:
         """
@@ -130,17 +123,12 @@ class Process:
         else:
             return self.transformer(*args, **kwargs).transform(dataframe)
 
-    def load(self, df, es_indice: str,
-             es_instance: Optional[Elasticsearch] = None, *args, **kwargs) -> None:
+    def load(self, df, *args, **kwargs) -> None:
         """
         Loads the passed dataframe into the passed elasticsearch instance's indice es_indice.
 
         :param df: pd.Dataframe
             the dataframe to load
-        :param es_indice:
-            the elasticsearch indice in which to load
-        :param es_instance:
-            the elasticsearch instance in which to load
         :param args:
             optional positional parameters for custom loader instanciation
         :param kwargs:
@@ -150,31 +138,28 @@ class Process:
         if self.__loader_is_instanced:
             if len(args) + len(kwargs) > 0:
                 warnings.warn("Instanced loader receiving extra arguments !")
-            self.loader.load(df, es_indice) # noqa
+            self.loader.load(df)
         else:
-            assert isinstance(es_instance, Elasticsearch)
-            self.loader(es_instance, *args, **kwargs).load(df, es_indice)
+            self.loader(*args, **kwargs).load(df)
 
-    def bulk(self, file_indice_dic: Union[Dict[str, str], Dict[str, List[str]]]) -> None:
+    def bulk(self,  file_list: List[str]) -> None:
         """
-        Given a dict of format {file1: indice1, file2: indice2} or {indice1: [file1, file2], indice2: [file3, file4]}
-            extract and transform all files before loading into the corresponding indice.
+        Given a list of files, loads the files into the loader's indice.
             Only works for Process with instanced Extractors, Transformers and Loaders
 
         =======
         Example
-        >>> process = Process(CSVExtractor(), Transformer(), Loader())
-        >>> myconf = {"covid_stats.csv": "covid", "relance.xls": "relance", "obscure_name.xlsx": "id7654"}
+        >>> process = Process(CSVExtractor(), Transformer(), Loader({"user": "", "pwd": ""}, "my_indice"))
+        >>> myconf = ["covid_stats.csv", "relance.xls", "obscure_name.xlsx"]
         >>> process.bulk(myconf)
         Equivalent to
-        >>> for f, i in myconf:
-        ...     process.process(f, i)
+        >>> for i in myconf:
+        ...     process.process(i)
         Example in the second format
-        >>> my_second_conf = {"covid": ["covid_stats_2020.csv", "covid_stats_2021.csv"]}
+        >>> my_second_conf = ["covid_stats_2020.csv", "covid_stats_2021.csv"]
         >>> process.bulk(my_second_conf)
 
-        :param file_indice_dic: the dictionnary of parameters to use for extracting and loading. Must be in the format
-            {file1: indice1}
+        :param file_list: the list of files to be bulked into the loader's indice
         :return: None
         """
         try:
@@ -189,9 +174,5 @@ class Process:
                 else:
                     err = "Loader"
             raise ValueError(f"{err} not instanced")
-        for key, value in file_indice_dic.items():
-            if isinstance(value, list):
-                for file in value:
-                    self.process(file, key)
-            else:
-                self.process(key, value)
+        for file in file_list:
+            self.process(file)
