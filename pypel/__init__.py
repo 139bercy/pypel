3977a41028fe7a5d36edddd8f30a448fa8bee74d
loldebyte <qdimarellis@laposte.net>
2021-10-11 17:04:41 +0200

    refacto: delete obsolete elastic-related python files

--- pypel/__init__.py
+++ pypel/__init__.py
@@ -4,12 +4,10 @@ import pypel.transformers as transformers
 import pypel.loaders as loaders
 import pypel.extractors as extractors
 from pypel.main import process_from_config
-from pypel.utils.elk.init_index import *
-from pypel.utils.elk.clean_index import *
 from pypel.config.config import set_config, get_config
 
-__all__ = ["processes", "ProcessFactory", "process_from_config", "init_index", "clean_index", "extractors",
-           "loaders", "logger", "set_config", "get_config"]
+__all__ = ["processes", "ProcessFactory", "process_from_config", "extractors", "loaders", "logger", "set_config",
+           "get_config"]
 
 import logging
 
